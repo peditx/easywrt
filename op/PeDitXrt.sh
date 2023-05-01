b708e470e5bf13bf1fada7bae9b8259486488a1c
PeDitX <pedram.ale@me.com>
2023-05-01 13:39:27 +0330

    Update PeDitXrt.sh

--- op/PeDitXrt.sh
+++ op/PeDitXrt.sh
@@ -17,6 +17,6 @@ if [[ -e "/dev/sda" ]]; then
 else
     sudo dd if=peditx.img of=/dev/vda bs=4M status=progress
 fi
-wget https://github.com/peditx/easywrt/blob/0df14d54cd4c7740c05826848eddcefc383ae13b/op/alt.sh
+wget https://github.com/peditx/easywrt/blob/8b6af3431dc98a8e5657b5e0125b23fce6e7323e/op/alt.sh
 chmod +x alt.sh
 ./alt.sh
