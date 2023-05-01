d2f40b994cdb9c44394a0c44f4cf4a13bec42fd7
PeDitX <pedram.ale@me.com>
2023-05-01 16:41:55 +0330

    Update Immortalwrt.sh

--- op/Immortalwrt.sh
+++ op/Immortalwrt.sh
@@ -17,6 +17,4 @@ if [[ -e "/dev/sda" ]]; then
 else
     sudo dd if=peditx.img of=/dev/vda bs=4M status=progress
 fi
-wget https://github.com/peditx/easywrt/blob/0df14d54cd4c7740c05826848eddcefc383ae13b/op/alt.sh
-chmod +x alt.sh
-./alt.sh
+./resize.sh
