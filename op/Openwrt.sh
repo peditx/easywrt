5ac6edeb71d939fd3e93f8e10638612a99aa7fb8
PeDitX <pedram.ale@me.com>
2023-05-01 16:42:25 +0330

    Update Openwrt.sh

--- op/Openwrt.sh
+++ op/Openwrt.sh
@@ -17,22 +17,4 @@ if [[ -e "/dev/sda" ]]; then
 else
     sudo dd if=peditx.img of=/dev/vda bs=4M status=progress
 fi 
-
-echo "resizing Partition 2"
-
-# Resize the second partition to the desired size
-read -p "Enter the size of partition 2 (in GB): " size
-
-echo "Resizing partition 2..."
-sudo parted /dev/sda resizepart 2 "${size}GB"
-
-echo "Resizing file system of partition 2..."
-sudo resize2fs /dev/sda2
-
-echo "Ok done! thank you for using PeDitXrt installer. Subscribe to us on YouTube/Instagram/Telegram/Twitter @peditx"
-echo "Unmounting the USB flash drive..."
-sudo umount /dev/sdb
-
-echo "Rebooting the system in 5 seconds..."
-sleep 5
-sudo reboot
+ ./resize.sh
