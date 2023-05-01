8b6af3431dc98a8e5657b5e0125b23fce6e7323e
PeDitX <pedram.ale@me.com>
2023-05-01 13:38:57 +0330

    Update alt.sh

--- op/alt.sh
+++ op/alt.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
 echo "resizing Partition 2"
+
 # Resize the second partition to the desired size
-
 read -p "Enter the size of partition 2 (in GB): " size
-    echo "Resizing partition 2..."
-    parted /dev/sda resizepart 2 ${size}GB
-    echo "Resizing file system of partition 2..."
-    resize2fs /dev/sda2
 
+echo "Resizing partition 2..."
+sudo parted /dev/sda resizepart 2 "${size}GB"
 
-echo "Ok done! thank you for using PeDitXrt installer subscribe us on Youtube/Instagram/Telegram/Twitter @peditx"
+echo "Resizing file system of partition 2..."
+sudo resize2fs /dev/sda2
+
+echo "Ok done! thank you for using PeDitXrt installer. Subscribe to us on YouTube/Instagram/Telegram/Twitter @peditx"
 echo "Unmounting the USB flash drive..."
-
 sudo umount /dev/sdb
 
 echo "Rebooting the system in 5 seconds..."
 sleep 5
-reboot
+sudo reboot
