abc82d17fa95dcd1664b3356f2d4d2cba1c3f8ec
PeDitX <pedram.ale@me.com>
2023-05-27 16:51:54 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@ sudo apt-get upgrade
 sudo apt-get install gunzip gzip curl wget git
 
 # Download install.sh and resize.sh
-curl -O https://raw.githubusercontent.com/peditx/easywrt/36d3bf205b46c1d7b17bfbc7c2973b7a54887c39/install.sh
+curl -O https://github.com/peditx/easywrt/blob/8102844c3f2048ab790da1d3f481e611045516e0/install.sh
 curl -O https://raw.githubusercontent.com/peditx/easywrt/36d3bf205b46c1d7b17bfbc7c2973b7a54887c39/op/resize.sh
 curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Custom.sh
 curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Immortalwrt.sh
