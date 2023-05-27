1143e9420893aec848ebbcb95370b4ca4c64e3ef
PeDitX <pedram.ale@me.com>
2023-05-27 16:47:53 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,6 +22,11 @@ sudo apt-get install gunzip gzip curl wget git
 # Download install.sh and resize.sh
 curl -O https://raw.githubusercontent.com/peditx/easywrt/36d3bf205b46c1d7b17bfbc7c2973b7a54887c39/install.sh
 curl -O https://raw.githubusercontent.com/peditx/easywrt/36d3bf205b46c1d7b17bfbc7c2973b7a54887c39/op/resize.sh
+curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Custom.sh
+curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Immortalwrt.sh
+curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Mikrotik.sh
+curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Openwrt.sh
+curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/PeDitXrt.sh
 
 # Set execute permissions for install.sh and resize.sh
 chmod +x install.sh
