a50d2b9643c78273d300a3f9f25edc4f3d07b6e6
PeDitX <pedram.ale@me.com>
2024-11-12 18:54:19 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,12 +21,12 @@ sudo apt-get install gunzip gzip curl wget git
 
 # Download install.sh and resize.sh
 curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/install.sh
-curl -O https://raw.githubusercontent.com/peditx/easywrt/36d3bf205b46c1d7b17bfbc7c2973b7a54887c39/op/resize.sh
-curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Custom.sh
-curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Immortalwrt.sh
-curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Mikrotik.sh
-curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/Openwrt.sh
-curl -O https://github.com/peditx/easywrt/blob/5959810386d472eabeeba495f1798fdab658363d/op/PeDitXrt.sh
+curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/resize.sh
+curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/Custom.sh
+curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/Immortalwrt.sh
+curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/Mikrotik.sh
+curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/Openwrt.sh
+curl -O https://raw.githubusercontent.com/peditx/easywrt/refs/heads/main/op/PeDitXrt.sh
 
 # Set execute permissions for install.sh and resize.sh
 chmod +x install.sh
