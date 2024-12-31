20549bd8c4e66cea01468a9558c27463c762cec8
PeDitX <pedram.ale@me.com>
2024-12-31 18:28:24 +0330

    Update Immortalwrt.sh

--- op/Immortalwrt.sh
+++ op/Immortalwrt.sh
@@ -3,9 +3,9 @@ echo "Installing ImmortalWRT"
 # Download and install Openwrt
 CPU_ARCH=$(uname -m)
 if [[ "$CPU_ARCH" == "x86_64" ]]; then
-    DOWNLOAD_URL="https://downloads.immortalwrt.org/releases/23.05.4/targets/x86/64/immortalwrt-21.02.5-x86-64-generic-squashfs-combined.img.gz"
+    DOWNLOAD_URL="https://downloads.immortalwrt.org/releases/23.05.4/targets/x86/64/immortalwrt-23.05.4-x86-64-generic-squashfs-combined.img.gz"
 elif [[ "$CPU_ARCH" == "i686" ]]; then
-    DOWNLOAD_URL="https://downloads.immortalwrt.org/releases/23.05.4/targets/x86/generic/immortalwrt-21.02.5-x86-generic-generic-squashfs-combined.img.gz"
+    DOWNLOAD_URL="https://downloads.immortalwrt.org/releases/23.05.4/targets/x86/generic/immortalwrt-23.05.4-x86-generic-generic-squashfs-combined.img.gz"
 else
     echo "Unsupported CPU architecture: $CPU_ARCH"
     exit 1
